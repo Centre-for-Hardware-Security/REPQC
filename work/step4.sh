e6f98ffa56935cffd319bf9b829556093d31c1f8
Samuel Pagliarini <50336652+snpagliarini@users.noreply.github.com>
2023-05-10 10:28:04 +0300

    fixing step4.sh

--- work/step4.sh
+++ work/step4.sh
@@ -1,1 +1,1 @@
-../bin/relic_pca --net ../bench/kali.bench --out ../results/kali.grouping --cmps 64
+../bin/relic_pca --net ../bench/kali.tool1.bench --out ../results/kali.tool1.grouping --feat ../aux/features.groups.txt
