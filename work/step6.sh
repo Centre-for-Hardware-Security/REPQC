1a0fb9339e0ee6f0ec362871897fbabfe4c3d2a8
Samuel Pagliarini <50336652+snpagliarini@users.noreply.github.com>
2023-07-26 10:35:55 +0300

    added comments on how to run step6 for gmu design

--- work/step6.sh
+++ work/step6.sh
@@ -1,2 +1,6 @@
 echo $1
-../bin/verifier ../results/$1.zscore KeccakCore_state_out_
+../bin/verifier ../results/$1.zscore $2 $3
+
+#/step6.sh saber_v2 SH_KeccakCore_state_out_ SH_KeccakCore_Rounds_lanes_out_
+#./step6.sh gmu KECCAK_datapath_gen_to_round_ KECCAK_datapath_gen_from_sipo
+
