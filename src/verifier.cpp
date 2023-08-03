80b877949077b4a98aebdf6f9fd97973405c78d6
Samuel Pagliarini <50336652+snpagliarini@users.noreply.github.com>
2023-08-03 13:11:33 +0300

    verifier now outputs a csv file for easy plotting of Z-scores

--- src/verifier.cpp
+++ src/verifier.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-void parseZscore(string input, string base);
+void parseZscore(string input, string base, string base2);
 
 struct reg {
 	int id;
@@ -13,29 +13,37 @@ struct reg {
 };
 
 int main(int argc, char** argv) 
-{ 
-	if (argc != 3) {
+{
+	if (argc != 4) {
 		cout << "wrong number of parameters" << std::endl;
-		cout << "the correct usage is " << argv[0] << "file.zscore Keccak_reg_basename" << std::endl;
-		cout << "Example: " << argv[0] << " input.zscore KeccakCore_state_out_" << std::endl;
+		cout << "the correct usage is " << argv[0] << "file.zscore Keccak_reg_basename target_base_name" << std::endl;
+		cout << "Example: " << argv[0] << " input.zscore KeccakCore_state_out_ doutb_3" << std::endl;
 		return 0;
 	}
 
+	cout << "the verifier will also generate a .csv file for easy visualization" << std::endl;
+
 	string input = argv[1];
 	string base = argv[2];
+	string base2 = argv[3];
 	
-	parseZscore(input, base);
+	parseZscore(input, base, base2);
 
 	return 0; 
 }
 
-void parseZscore(string input, string base) {
+void parseZscore(string input, string base, string base2) {
 	float lowest = 999.9;
 	float highest = 0.0;
 	string lowest_name = "UNK";
 	string highest_name = "UNK";
 	float sum = 0.0;
 	int count = 0;
+
+	ofstream csv;
+	csv.open ("../work/csv.csv");
+
+	csv << "id, z-score, label" << std::endl;
 
 	std::ifstream file(input);
 	if (!file) {
@@ -49,6 +57,7 @@ void parseZscore(string input, string base) {
 	string part1, part2, part3;
 	int hit = 0; 
 	int miss = 0;
+	int id = 0;
 
 	while(file) {
 		file >> part1;
@@ -65,6 +74,7 @@ void parseZscore(string input, string base) {
 			myreg.id = stoi(part1);
 			myreg.score = stof(part2);
 			myreg.name = part3;
+			id++;
 
 			if (myreg.name.find(base) != std::string::npos) { // meaning that base is a substring of the reg name
 				if (myreg.score < lowest) {
@@ -80,9 +90,15 @@ void parseZscore(string input, string base) {
 
 				cout << "found " << part3 << ", id of " << part1 << " and score of " << part2;
 				hit++;
-				cout << " (" << hit << " out of " << (hit+miss) << ")" << endl;				
+				cout << " (" << hit << " out of " << (hit+miss) << ")" << endl;
+				csv << id << ", " << part2 << ", state" << std::endl;
+			}
+			else if (myreg.name.find(base2) != std::string::npos) {
+				csv << id << ", " << part2 << ", target" << std::endl;
+				miss++;
 			}
 			else {
+				csv << id << ", " << part2 << ", other" << std::endl;
 				miss++;
 			}
 		}
